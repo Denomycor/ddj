4fce0e09e73e3b22d092eff34293008c3b706ac6
Denomycor <afonsoresteves@hotmail.com>
2025-01-16 13:16:50 +0000

    Create agent navigation for enemies

---
+++ assets/scripts/enemy_states/chase_state.gd
@@ -0,0 +1,50 @@
+class_name ChaseState extends State
+
+
+var troop: EnemyTroop
+var target: Node2D
+var nav_agent: NavigationAgent2D
+
+
+func prepare() -> void:
+	troop = owner as EnemyTroop
+	nav_agent = troop.get_node("NavigationAgent2D")
+
+
+func enter(_previous_state: State, args) -> void:
+	assert(args is Node2D)
+	target = args
+	# nav_agent.velocity_computed.connect(_on_velocity_computed)
+	target.get_node("HitboxComponent").died.connect(leave_state)
+	calculate_path()
+	nav_agent.get_node("Timer").timeout.connect(calculate_path)
+	nav_agent.get_node("Timer").start()
+
+
+func physics_process(_delta: float) -> void:
+	if(!nav_agent.is_navigation_finished()):
+		var next_point := nav_agent.get_next_path_position()
+		var motion: Vector2 = troop.global_position.direction_to(next_point) * troop.speed
+		# nav_agent.set_velocity(motion)
+		_on_velocity_computed(motion)
+
+
+func exit(_next_state: State) -> void:
+	nav_agent.velocity_computed.disconnect(_on_velocity_computed)
+	nav_agent.get_node("Timer").stop()
+	nav_agent.get_node("Timer").timeout.disconnect(calculate_path)
+	target.get_node("HitboxComponent").died.disconnect(leave_state)
+
+
+func _on_velocity_computed(velocity: Vector2) -> void:
+	troop.velocity = velocity
+	troop.move_and_slide()
+
+
+func calculate_path() -> void:
+	nav_agent.set_target_position(target.global_position)
+
+
+func leave_state() -> void:
+	pass
+
--- assets/scripts/enemy_troop.gd
+++ assets/scripts/enemy_troop.gd
@@ -1,9 +1,44 @@
 class_name EnemyTroop extends CharacterBody2D
 
 @onready var hitbox_component: HitboxComponent = $HitboxComponent
+@onready var perception_component: PerceptionComponent = $PerceptionComponent
+@onready var attack_range_component: PerceptionComponent = $attack_range
 
+@export var speed = 100;
+
+var state_machine := StateMachine.new(self)
 
 func _ready() -> void:
 	hitbox_component.died.connect(queue_free)
 	hitbox_component.hit.connect(print)
 
+	state_machine.add_state(State.new("idle"))
+	state_machine.starting_state("idle")
+
+
+func _physics_process(delta: float) -> void:
+	state_machine.physics_process(delta)
+
+
+func get_player_troops() -> Array[Node2D]:
+	return get_tree().get_nodes_in_group("player_troops") as Array[Node2D]
+
+
+func transition(state: State) -> void:
+	var ranger_list := perception_component.enemies_in_range.filter(func(e): return e.designation == "ranger")
+	var has_ranger := !ranger_list.is_empty()
+
+	# If a ranger is in range attack it
+	if(has_ranger):
+		var _nearest_ranger: Array[Node2D] = ranger_list.reduce(func(acc: Node2D, e: Node2D):
+			return e if is_first_closer(e.global_position, acc.global_position) else acc
+		, ranger_list[0])
+		state_machine.transition(state, "chase_ranger")
+
+	# If no ranger is in range attack any troop
+	else:
+		state_machine.transition(state, "attack")
+
+
+func is_first_closer(st: Vector2, nd: Vector2) -> bool:
+	return global_position.distance_squared_to(st) < global_position.distance_squared_to(nd)
